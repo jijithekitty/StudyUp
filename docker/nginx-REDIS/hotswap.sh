831ee687d6307d6ff74de47b1e636f69166c0cb4
Jenny Nguyen <jenngu@ucdavis.edu>
2019-03-14 00:26:47 +0000

    Finalized homework and extra credit.

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -1,32 +1,35 @@
-#set environment
+# Set environment
 set -e
 
-#Check that PARAMS was passed
+# Check that parameter was passed
 if [ -z "$1" ]; then
     echo "No parameter passed to script"
     exit 1
 fi
 
-#location of nginx.conf file
+# Location of nginx.conf file
 config="/etc/nginx/nginx.conf"
 
-#Get the current redis url on line 11 of config file:
+# Get the current redis url on line 11 of config file:
 lineLocationURL=$(sed -n  '11p' $config)
 
-#extract content before :6379 port number
-#server 34.73.56.224:6379;
+# Extract content before :6379 port number
+# Server 34.73.56.224:6379;
 beforePort=${lineLocationURL%:6379*}
 
-#store server name, removing server from "server name"
-hostName=${beforePort:11}
+# Extract host name after server text, 'server host'
+host=$(echo $beforePort | awk -F 'server' '{print $2}')
+
+# Trim whitespaces from host name after extracting
+hostName=$(echo ${host//[[:space:]]})
 
 # Current Hostname
 echo "Current Host: $hostName"
 
-#if param doesn't match host name, hotswap and replace host name with param
-#reload nginx after swapping hostname
+# If param doesn't match host name, hotswap and replace host name with param
+# Reload nginx after swapping hostname
 if [ "$1" != "$hostName" ]; then
-	echo "Swapping hostnames"
+	echo "Swapping Hostnames"
 	sed -i "s/server.*:/server $1:/g" $config
 	/usr/sbin/nginx -s reload
 else
